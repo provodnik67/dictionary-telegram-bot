25d7ca442b1620945e97437cf58c063d7979681d
Fedor Kataev <fkataev@corp.local>
2026-06-16 22:50:21 +0300

    flock is removed

--- getUpdatesCLI.php
+++ getUpdatesCLI.php
@@ -70,16 +70,6 @@ if(
     }
 }
 
-$lockHandle = fopen(__DIR__ . '/getUpdatesCLI.lock', 'c');
-if ($lockHandle === false) {
-    fwrite(STDERR, date('Y-m-d H:i:s') . " - Cannot open lock file\n");
-    exit(1);
-}
-if (!flock($lockHandle, LOCK_EX | LOCK_NB)) {
-    fclose($lockHandle);
-    exit(0);
-}
-
 $telegram = new Longman\TelegramBot\Telegram(Config::get('bot.api_key'), Config::get('bot.username'));
 $telegram->addCommandsPaths([__DIR__ . '/Commands']);
 $telegram->addCommandsPaths([__DIR__ . '/BaseCommands']);
